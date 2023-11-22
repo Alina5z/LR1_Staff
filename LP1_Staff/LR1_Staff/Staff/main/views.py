b74cd64953f27b26a10ba32ef2c7941460348c17
Alina5z <117967981+Alina5z@users.noreply.github.com>
2023-11-22 10:42:26 +0300

    Add files via upload

---
+++ LP1_Staff/LR1_Staff/Staff/main/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from .models import Staf
+from .forms import StafForm
+
+def index(request):
+    staf = Staf.objects.order_by('id')
+    return render(request, 'main/index.html', {'title': 'Главная страница сайта',
+                                               'staf': staf})
+
+def order(request):
+    return render(request, 'main/order.html')
+
+def add(request):
+    error = ""
+    if request.method == 'POST':
+        form = StaffForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма была неверной'
+
+    form = StaffForm()
+    context = {'form': form, 'error': error}
+    return render(request, 'main/add.html', context)
